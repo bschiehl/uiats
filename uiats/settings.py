1cc2ac6c525030b7e702f3843b1e8beea69ee055
Bernhard Schiehl <bernhard.schiehl@chello.at>
2021-01-05 17:09:01 +0100

    security updates

--- uiats/settings.py
+++ uiats/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '=-5+c_90)=@7^pk%fhi$&!x)kro()!8f$9+iuc92bm9mkof095'
-
+SECRET_KEY = os.environ['SECRET_KEY']
+        
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
