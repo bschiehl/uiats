0ba992ca4b2f09bdfd725554a31c5baf2d781b39
Bernhard Schiehl <bernhard.schiehl@chello.at>
2021-02-19 14:23:01 +0100

    fix allowed_hosts

--- uiats/settings.py
+++ uiats/settings.py
@@ -24,9 +24,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ['SECRET_KEY']
         
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS')
+
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS').split(" ") 
 
 
 # Application definition
